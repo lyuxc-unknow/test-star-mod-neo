7d2c63d86dd191bbcf91ebaa58e41a75db0f71ef
lyuxc-unknow <2675796669@qq.com>
2023-11-27 20:22:49 +0800

    add mixin file (The Nether Portal can only be ignited at The End!)

---
+++ src/main/java/me/lyuxc/develop/mixins/MixinBaseFireBlock.java
@@ -0,0 +1,16 @@
+package me.lyuxc.develop.mixins;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BaseFireBlock;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(BaseFireBlock.class)
+public class MixinBaseFireBlock {
+    @Inject(method = "inPortalDimension", at = @At("RETURN"), cancellable = true)
+    private static void inPortalDimension(Level p_49249_, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(p_49249_.dimension() == Level.END);
+    }
+}
