ea8ea71e34afe69bea8c1aeec878ef33c45c043d
lyuxc-unknow <2675796669@qq.com>
2024-02-10 18:29:41 +0800

    hide advancement toast

---
+++ src/main/java/me/lyuxc/develop/mixins/AdvancementToastMixin.java
@@ -0,0 +1,18 @@
+package me.lyuxc.develop.mixins;
+
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.toasts.AdvancementToast;
+import net.minecraft.client.gui.components.toasts.Toast;
+import net.minecraft.client.gui.components.toasts.ToastComponent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(AdvancementToast.class)
+public class AdvancementToastMixin {
+    @Inject(method = "render",at = @At("RETURN"), cancellable = true)
+    public void render$mixin(GuiGraphics pGuiGraphics, ToastComponent pToastComponent, long pTimeSinceLastVisible, CallbackInfoReturnable<Toast.Visibility> cir) {
+        cir.setReturnValue(Toast.Visibility.HIDE);
+    }
+}
