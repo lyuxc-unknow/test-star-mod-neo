8a4ce3ae5bd88987c6409563e2cc51c80ec58af8
lyuxc-unknow <2675796669@qq.com>
2024-02-10 18:37:32 +0800

    rename

--- src/main/java/me/lyuxc/develop/mixins/MixinAdvancementToast.java
+++ src/main/java/me/lyuxc/develop/mixins/MixinAdvancementToast.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AdvancementToast.class)
-public class AdvancementToastMixin {
+public class MixinAdvancementToast {
     @Inject(method = "render",at = @At("RETURN"), cancellable = true)
     public void render$mixin(GuiGraphics pGuiGraphics, ToastComponent pToastComponent, long pTimeSinceLastVisible, CallbackInfoReturnable<Toast.Visibility> cir) {
         cir.setReturnValue(Toast.Visibility.HIDE);
