9ff1b904a4efc0c1020540d39b44737d5aab96b3
lyuxc-unknow <2675796669@qq.com>
2024-02-25 11:12:18 +0800

    更新版本，添加末地传送门框架嵌入末影之眼的效果

--- src/main/java/me/lyuxc/develop/event/onPlayerInteract.java
+++ src/main/java/me/lyuxc/develop/event/onPlayerInteract.java
@@ -1,11 +1,22 @@
 package me.lyuxc.develop.event;
 
 import me.lyuxc.develop.Variables;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LightningBolt;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
@@ -16,11 +27,27 @@ public class onPlayerInteract {
     public static void onPlayerRightBlock(PlayerInteractEvent.RightClickBlock event) {
         Player player = event.getEntity();
         Level level = event.getLevel();
-        BlockState blockState = level.getBlockState(event.getPos());
+        BlockPos pos = event.getPos();
+        BlockState blockState = level.getBlockState(pos);
         for(String s : Variables.IDs) {
             if(BuiltInRegistries.BLOCK.getKey(blockState.getBlock()).toString().equals(s)) {
                 player.displayClientMessage(Component.translatable("ts.tips.right.disable",s),true);
                 event.setCanceled(true);
+            }
+        }
+        if(blockState.getBlock() == Blocks.END_PORTAL_FRAME) {
+            if(!blockState.getValue(BlockStateProperties.EYE) && player.getItemInHand(InteractionHand.MAIN_HAND).is(Items.ENDER_EYE)) {
+                if(level instanceof ServerLevel level_) {
+                    if(player.getItemBySlot(EquipmentSlot.OFFHAND).is(BuiltInRegistries.ITEM.get(new ResourceLocation("test_star:mod_block_dream")))) {
+                        player.getItemBySlot(EquipmentSlot.OFFHAND).setCount(player.getItemBySlot(EquipmentSlot.OFFHAND).getCount() - 1);
+                        Entity light = new LightningBolt(EntityType.LIGHTNING_BOLT, level_);
+                        light.moveTo(player.position());
+                        player.sendSystemMessage(Component.literal("§k welcome to the end"));
+                        level_.addFreshEntity(light);
+                    } else {
+                        event.setCanceled(true);
+                    }
+                }
             }
         }
     }
