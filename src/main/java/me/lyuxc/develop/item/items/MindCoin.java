19e7955c6847ab2c6b1ff9d34708b2977f5b7421
lyuxc-unknow <2675796669@qq.com>
2024-04-06 14:03:36 +0800

    feat: ✨ Lucky Coin

--- src/main/java/me/lyuxc/develop/item/items/MindCoin.java
+++ src/main/java/me/lyuxc/develop/item/items/MindCoin.java
@@ -13,9 +13,12 @@ public class MindCoin extends Item {
 
     @Override
     public boolean onDroppedByPlayer(@NotNull ItemStack item, @NotNull Player player) {
-        if (Math.random() * 100 > 80) {
-            item.setCount(item.getCount() - 1);
+        double r = Math.random() * 100;
+        item.setCount(item.getCount() - 1);
+        if (r > 80) {
             player.drop(Utils.getItemStack("magic_clover:magic_clover"), false);
+        } else if (r > 50) {
+            item.setCount(item.getCount() + 1);
         }
         return super.onDroppedByPlayer(item, player);
     }
